73623e1a06036bb470748f137160d87424e39ec0
Brian McConney <briancamcc@gmail.com>
2022-11-17 18:38:38 -0400

    35 mins into Arrays

---
+++ src/main/java/ArrayAndArrayList/Arrays.java
@@ -0,0 +1,17 @@
+package ArrayAndArrayList;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        //ways to declare Arrays
+        // int[] arr = new int[5];
+        // int[] arr2 = {1,2,3,4,5};
+
+     //   int[] ros; // declaration of array. ros is defined in the stack
+     //   ros = new int[5]; //Actual object is created in heap memory. Given default values of 0
+
+            String[] arr = new String[5]; // given a default value of null
+
+             System.out.println(arr[0]);
+    }
+}
---
+++ src/main/java/ArrayAndArrayList/Input.java
@@ -0,0 +1,36 @@
+package ArrayAndArrayList;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr=  new int[5];
+
+        arr[0] = 23;
+        arr[1] = 233;
+        arr[2] = 238;
+        arr[3] = 123;
+        arr[4] = 423;
+
+        System.out.println(arr[3]);
+
+        System.out.println(Arrays.toString(arr));
+
+        //input using for loops
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        //output using for loops
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        //output using for each loops
+        for (int elem : arr) {
+            System.out.println(elem);
+        }
+    }
+}
